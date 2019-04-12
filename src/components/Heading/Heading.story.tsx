f3e6c80b2bc48b88a76cc90efc0111e414e7dec4
mNutella <netcore.click@gmail.com>
2019-04-12 23:31:27 +0500

    The heading was refactoring

--- src/components/Heading/Heading.story.tsx
+++ src/components/Heading/Heading.story.tsx
@@ -3,13 +3,30 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Heading } from ".";
 
-storiesOf("Heading", module).add("default", () => ( 
-  <div>
+storiesOf("Heading", module).add("types", () => ( 
+  <div className="bd-notification is-dark">
     <Heading>Default</Heading>
     <Heading subtitle>Subtitle</Heading>
     <Heading weight="bold">Text bold</Heading>
     <Heading subtitle weight="bold">Subtitle</Heading>
     <Heading size={5}>Default</Heading>
     <Heading size={5} subtitle>Subtitle</Heading>
+  </div>
+));
+
+storiesOf("Heading", module).add("size", () => ( 
+  <div className="bd-notification is-dark">
+    <Heading size={1}>Size is 1</Heading>
+    <Heading size={2}>Size is 2</Heading>
+    <Heading size={3}>Size is 3</Heading>
+    <Heading size={4}>Size is 4</Heading>
+    <Heading size={5}>Size is 5</Heading>
+    <Heading size={6}>Size is 6</Heading>
+    <Heading size={1} subtitle>Size is 1</Heading>
+    <Heading size={2} subtitle>Size is 2</Heading>
+    <Heading size={3} subtitle>Size is 3</Heading>
+    <Heading size={4} subtitle>Size is 4</Heading>
+    <Heading size={5} subtitle>Size is 5</Heading>
+    <Heading size={6} subtitle>Size is 6</Heading>
   </div>
 ));
--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,17 +1,35 @@
 import styled from "../../theme/styled-components";
+import theme from "../../theme";
 
-interface IProps {
+/**
+ * Default properties.
+ *
+ * @type {Object} heading
+ */
+
+type Props = {
+  color?: keyof typeof theme.colors;
   subtitle?: boolean;
   size?: 1 | 2 | 3 | 4 | 5 | 6;
   weight?: 'lighter' | 'normal' | 'bold';
 }
 
-export const Heading = styled.h1<IProps>`
-  color: ${(props) => props.theme.colors.first};
+/**
+ * A heading.
+ *
+ * @type {Component}
+ */
+
+export const Heading = styled.h1<Props>`
+  color: ${(props) => props.color ? 
+    props.theme.colors[props.color] : 
+    props.theme.colors.first};
   font-size: ${(props) => props.theme.fontSizes[(props.size || 1) - 1]}rem;
-  line-height: normal;
-  text-transform: lowercase;
+  line-height: ${(props) => props.theme.heading.line_height};
+  text-transform: ${(props) => props.theme.heading.text_transform};
   font-weight: ${(props) => props.weight || 'lighter'};
+  font-style: ${(props) => props.theme.heading.font_style};
+  letter-spacing: ${(props) => props.theme.heading.letter_spacing};
 
   ${(props) => props.subtitle && `
     font-size: ${props.theme.fontSizes[(props.size || 1) - 1] / 1.5}rem;
