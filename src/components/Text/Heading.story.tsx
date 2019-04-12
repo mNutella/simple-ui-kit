2ff049111e3194a73b6d50c6a6e79ff1dc1bd140
mNutella <netcore.click@gmail.com>
2019-04-13 00:03:05 +0500

    The text was refactoring

---
+++ src/components/Text/Heading.story.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { Text } from ".";
+
+storiesOf("Text", module).add("types", () => ( 
+  <div className="bd-notification is-dark">
+    <Text>Default</Text>
+    <Text subtext>Subtitle</Text>
+    <Text weight="bold">Text bold</Text>
+    <Text subtext weight="bold">Subtitle</Text>
+    <Text size={5}>Default</Text>
+    <Text size={5} subtext>Subtitle</Text>
+  </div>
+));
+
+storiesOf("Text", module).add("size", () => ( 
+  <div className="bd-notification is-dark">
+    <Text size={1}>This is just a text about nothing with size is 1</Text>
+    <Text size={2}>This is just a text about nothing with size is 2</Text>
+    <Text size={3}>This is just a text about nothing with size is 3</Text>
+    <Text size={4}>This is just a text about nothing with size is 4</Text>
+    <Text size={5}>This is just a text about nothing with size is 5</Text>
+    <Text size={6}>This is just a text about nothing with size is 6</Text>
+    <Text size={1} subtext>This is just a text about nothing with size is 1</Text>
+    <Text size={2} subtext>This is just a text about nothing with size is 2</Text>
+    <Text size={3} subtext>This is just a text about nothing with size is 3</Text>
+    <Text size={4} subtext>This is just a text about nothing with size is 4</Text>
+    <Text size={5} subtext>This is just a text about nothing with size is 5</Text>
+    <Text size={6} subtext>This is just a text about nothing with size is 6</Text>
+  </div>
+));
---
+++ src/components/Text/index.tsx
@@ -0,0 +1,36 @@
+import styled from "../../theme/styled-components";
+import theme from "../../theme";
+
+/**
+ * Default properties.
+ *
+ * @type {Object} text
+ */
+
+type Props = {
+  color?: keyof typeof theme.colors;
+  subtext?: boolean;
+  size?: 1 | 2 | 3 | 4 | 5 | 6;
+  weight?: 'lighter' | 'normal' | 'bold';
+}
+
+/**
+ * A text.
+ *
+ * @type {Component}
+ */
+
+export const Text = styled.p<Props>`
+  font-family: ${(props) => props.theme.fonts.montserrate};
+  color: ${(props) => props.color ? 
+    props.theme.colors[props.color] : 
+    props.theme.colors.first};
+  font-size: ${(props) => props.theme.fontSizes[(props.size || 1) - 1]}rem;
+  line-height: ${(props) => props.theme.text.line_height};
+  font-weight: ${(props) => props.weight || 'lighter'};
+  letter-spacing: ${(props) => props.theme.text.letter_spacing};
+
+  ${(props) => props.subtext && `
+    font-size: ${props.theme.fontSizes[(props.size || 1) - 1] / 1.5}rem;
+  `}
+`;
