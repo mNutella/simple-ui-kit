5d5f6e16b58d2f497af9759f49855c5672900d08
mNutella <netcore.click@gmail.com>
2019-04-16 19:08:06 +0500

    The Image was refactoring

---
+++ src/elements/Image/Image.story.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { Image } from ".";
+
+const rations = ['square', '1by1', '4by3', '3by2', '16by9', '2by1', '5by4', '5by3', '3by1', '4by5', '3by4', '2by3', '3by5', '9by16', '1by2', '1by3']; 
+const sizes = [16, 24, 32, 48, 64, 96, 128]; 
+
+storiesOf("Image", module).add("default", () => (
+  <div style={{ width: 320 }}>
+    <Image src="https://bit.ly/2DrK18H" rounded ration="3by2" />
+  </div>
+));
+
+storiesOf("Image", module).add("Fixed Square", () => (
+  <table>
+    <thead>
+      <tr>
+        <th />
+        <th>Size</th>
+        <th>Image</th>
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      {sizes.map(size => (
+        <tr key={size}>
+          <td />
+          <td style={{ width: 100 }}>{size}</td>
+          <td style={{ width: 128 }}>
+            <Image
+              src="https://bit.ly/2If2zfN"
+              size={size}
+            />
+          </td>
+          <td />
+        </tr>
+      ))}
+    </tbody>
+  </table>
+));
+
+storiesOf("Image", module).add("Responsive images with ratios", () => (
+  <table>
+    <thead>
+      <tr>
+        <th />
+        <th>Size</th>
+        <th>Image</th>
+        <th />
+      </tr>
+    </thead>
+    <tbody>
+      {rations.map(ration => (
+        <tr key={ration}>
+          <td />
+          <td style={{ width: 100 }}>{ration}</td>
+          <td style={{ width: 128 }}>
+            <Image
+              src="https://bit.ly/2If2zfN"
+              ration={ration}
+            />
+          </td>
+          <td />
+        </tr>
+      ))}
+    </tbody>
+  </table>
+));
---
+++ src/elements/Image/index.tsx
@@ -0,0 +1,143 @@
+import * as React from "react";
+import styled from "../../theme/styled-components";
+import { overlay } from "../../utilities/mixins";
+
+/**
+ * Default properties.
+ *
+ * @type {Object} icon
+ */
+
+type Props = {
+  src?: string;
+  alt?: string;
+  rounded?: boolean;
+  ration?: 'square' | '1by1' | '4by3' | '3by2' | '16by9' | '2by1' | '5by4' | '5by3' | '3by1' | '4by5' | '3by4' | '2by3' | '3by5' | '9by16' | '1by2' | '1by3' | string;
+  size?: 16 | 24 | 32 | 48 | 64 | 96 | 128 | number;
+  preload?: boolean;
+  loaded?: boolean;
+  onLoad?: () => void;
+}
+
+/**
+ * A styled image container.
+ *
+ * @type {Component}
+ */
+
+export const StyledContainer = styled.figure<Props>`
+  display: ${(props) => props.theme.image.display};
+  position: ${(props) => props.theme.image.position};
+
+  img {
+    display: ${(props) => props.theme.image.img_display};
+    height: ${(props) => props.theme.image.img_height};
+    width: ${(props) => props.theme.image.img_width}%;
+    z-index: ${(props) => props.theme.image.img_z_index};
+    
+    ${(props) => props.rounded && `
+      border-radius: ${props.theme.radius.large};
+    `}
+      
+    ${(props) => props.preload && `
+      position: ${props.theme.image.img_preload_position};
+      z-index: ${props.theme.image.img_preload_z_index};
+      opacity: ${props.theme.image.img_preload_opacity};
+      filter: ${props.theme.image.img_preload_filter};
+      transition: ${props.theme.image.img_preload_transition};
+    `}
+
+    ${(props) => props.loaded && `
+      ${props.preload && `
+        opacity: ${props.theme.image.img_loaded_opacity};
+      `}
+    `}
+  }
+
+  ${(props) => props.ration && `
+    img {
+      ${overlay()};
+      height: ${props.theme.image.img_ration_height}%;
+      width: ${props.theme.image.img_ration_width}%;
+    }
+  `}
+
+  ${(props) => (props.ration == 'square' || props.ration == '1by1') && `
+    padding-top: ${props.theme.image.img_ration_1by1_pt}%;
+  `}
+
+  ${(props) => props.ration == '5by4' && `
+    padding-top: ${props.theme.image.img_ration_5by4_pt}%;
+  `}
+
+  ${(props) => props.ration == '4by3' && `
+    padding-top: ${props.theme.image.img_ration_4by3_pt}%;
+  `}
+
+  ${(props) => props.ration == '3by2' && `
+    padding-top: ${props.theme.image.img_ration_3by2_pt}%;
+  `}
+
+  ${(props) => props.ration == '5by3' && `
+    padding-top: ${props.theme.image.img_ration_5by3_pt}%;
+  `}
+
+  ${(props) => props.ration == '16by9' && `
+    padding-top: ${props.theme.image.img_ration_16by9_pt}%;
+  `}
+
+  ${(props) => props.ration == '2by1' && `
+    padding-top: ${props.theme.image.img_ration_2by1_pt}%;
+  `}
+
+  ${(props) => props.ration == '3by1' && `
+    padding-top: ${props.theme.image.img_ration_3by1_pt}%;
+  `}
+
+  ${(props) => props.ration == '4by5' && `
+    padding-top: ${props.theme.image.img_ration_4by5_pt}%;
+  `}
+
+  ${(props) => props.ration == '3by4' && `
+    padding-top: ${props.theme.image.img_ration_3by4_pt}%;
+  `}
+
+  ${(props) => props.ration == '2by3' && `
+    padding-top: ${props.theme.image.img_ration_2by3_pt}%;
+  `}
+
+  ${(props) => props.ration == '3by5' && `
+    padding-top: ${props.theme.image.img_ration_3by5_pt}%;
+  `}
+
+  ${(props) => props.ration == '9by16' && `
+    padding-top: ${props.theme.image.img_ration_9by16_pt}%;
+  `}
+
+  ${(props) => props.ration == '1by2' && `
+    padding-top: ${props.theme.image.img_ration_1by2_pt}%;
+  `}
+
+  ${(props) => props.ration == '1by3' && `
+    padding-top: ${props.theme.image.img_ration_1by3_pt}%;
+  `}
+
+  ${(props) => props.size && `
+    height: ${props.size}px;
+    width: ${props.size}px;
+  `}
+`
+
+/**
+ * A image.
+ *
+ * @type {Component}
+ */
+
+export const Image: React.FC<Props> = (props) => {
+  return (
+    <StyledContainer {...props}>
+      <img src={props.src} alt={props.alt} />
+    </StyledContainer>
+  )
+}
