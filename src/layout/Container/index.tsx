b0127cbc832890a88cca6c2b59b4b315f9926c38
mNutella <netcore.click@gmail.com>
2019-04-17 20:59:22 +0500

    The Container was refactoring

--- src/layout/Container/index.tsx
+++ src/layout/Container/index.tsx
@@ -1,46 +1,62 @@
 import styled  from "../../theme/styled-components";
 import { media, mediaMax } from "../../utilities/mixins";
 
-interface IProps {
+/**
+ * Default properties.
+ *
+ * @type {Object} container
+ */
+
+type Props = {
   fluid?: boolean;
   breakpoint?: 'widescreen' | 'fullhd';
 }
 
-// If the 'fluid' work incorrect, need to add into a first 'media.desktop' function of 'fluid' properties. 
-export const Container = styled.div<IProps>`
-  margin: 0 auto;
-  position: relative;
-  background-color: aqua;
+/**
+ * A container.
+ *
+ * @type {Component}
+ */
+
+// If the 'fluid' work incorrect, then need to add into a first 'media.desktop' function of 'fluid' properties. 
+export const Container = styled.div<Props>`
+  margin: ${(props) => props.theme.container.margin};
+  position: ${(props) => props.theme.container.position};
+
   ${(props) => media.desktop`
     max-width: ${props.theme.size.desktop.width}px;
     width: ${props.theme.size.desktop.width}px;
   `}
+
   ${(props) => media.widescreen`
     max-width: ${props.theme.size.widescreen.width}px;
     width: ${props.theme.size.widescreen.width}px;
   `}
+
   ${(props) => media.fullhd`
     max-width: ${props.theme.size.fullhd.width}px;
     width: ${props.theme.size.fullhd.width}px;
   `}
+
   ${({breakpoint}) => mediaMax.widescreen`
     ${(props) => (breakpoint == "widescreen") && `
       max-width: ${props.theme.size.widescreen.width}px;
-      width: auto;
+      width: ${props.theme.container.widescreen_width};
     `}
   `}
+
   ${({breakpoint}) => mediaMax.fullhd`
     ${(props) => (breakpoint == "fullhd") && `
       max-width: ${props.theme.size.fullhd.width}px;
-      width: auto;
+      width: ${props.theme.container.fullhd_width};
     `}
   `}
+
   ${({fluid}) => media.desktop`
     ${(props) => fluid && `
-      margin-left: ${props.theme.indents[0]};
-      margin-right: ${props.theme.indents[0]};
-      max-width: none;
-      width: auto;
+      margin: 0 ${props.theme.indents[0]};
+      max-width: ${props.theme.container.desktop_max_width};
+      width: ${props.theme.container.desktop_width};
     `}
   `}
 `;
